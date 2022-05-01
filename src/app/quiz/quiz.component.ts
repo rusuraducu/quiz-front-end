00d6096a444ee14f17867800b3989fd79c3e25d2
rusuraducu <rusu.raducu@yahoo.com>
2022-05-01 20:17:04 +0300

    1 file modified, 1 deleted

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  styleUrls: ['./quiz.component.scss'],
 })
 export class QuizComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
